f478cbc666b12ab7e04a1ade4978b40b6d457a09
Justin Lee <justinolee@utexas.edu>
2023-02-27 19:32:56 -0600

    Test commit, added a label

--- CartSquad/ViewController.swift
+++ CartSquad/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var splashLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        splashLabel.text = "Cart Squad"
     }
 
 
