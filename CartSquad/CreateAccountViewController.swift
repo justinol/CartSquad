7224c1e7a8e497c54ae77d67e43f4904206aa559
Angel Nevarez <angel.nevarez23@gmail.com>
2023-03-03 19:46:19 -0600

    new view controller

---
+++ CartSquad/CreateAccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateAccountViewController.swift
+//  CartSquad
+//
+//  Created by Angel Nevarez on 3/3/23.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
